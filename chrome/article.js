9e71c260db6be2b51390441cfbeeb64b1a8fde1d
sveco86 <sveco86@gmail.com>
2017-09-28 10:52:15 +0200

    Update article.js

--- chrome/article.js
+++ chrome/article.js
@@ -3,7 +3,7 @@
  */
 var utils = {};
 
-utils.whitelist = ['#text', 'BR', 'H1', 'H2', 'H3', 'P', 'DIV', 'SPAN', 'STRONG', 'SMALL', 'BLOCKQUOTE', 'UL', 'OL', 'LI', 'A', 'IMG'];
+utils.whitelist = ['#text', 'BR', 'H1', 'H2', 'H3', 'P', 'DIV', 'SPAN', 'STRONG', 'SMALL', 'BLOCKQUOTE', 'UL', 'OL', 'LI', 'A', 'IMG', 'BUTTON'];
 
 /**
  * Remove elements from document using selector
@@ -98,5 +98,5 @@ utils.getArticle = function(url) {
 };
 
 if (/\.sme\.sk\/c\/\d+\/.*/.test(document.location) && utils.isPianoArticle()) {
-	utils.getArticle('https://s.sme.sk/export/ma/?c=' + utils.articleId());
+	utils.getArticle('https://artemis.sme.sk/api/v2/article/' + utils.articleId() + '?mid=16&fullcont=1&noAdverts=0&nightmode=0');
 }
